4be5f4e57e0ee1537f6071adf9c37a1eac63d25a
anashasan <anashasan09@gmail.com>
2018-09-17 23:19:20 +0500

    Station Report Service

--- src/Host/DataModel/StationReportDto.cs
+++ src/Host/DataModel/StationReportDto.cs
@@ -7,18 +7,20 @@ namespace Host.DataModel
         public string Month { get; set; }
         public string Year { get; set; }
         public int Perform { get; set; }
+        public int isperform { get; set; }
         public string station { get; set; }
         public string Activity { get; set; }
         public string location { get; set; }
+        public string ActivityType { get; set; }
     }
 
     public class MonthlyPerform
     {
         public string Month { get; set; }
-        public int Perform { get; set; }
+        public int Value { get; set; }
     }
 
-    public class ActivityPerform
+    public class GraphActivityPerform
     {
         public string Activity { get; set; }
         public List<MonthlyPerform> MonthlyPerform { get; set; }
