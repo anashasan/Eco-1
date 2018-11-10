df997f7658cbedabc4d32a669eaa60de40d75cb8
Khalil Mohammad <khalilmohammadmirza@gmail.com>
2018-11-10 18:07:28 +0500

    Production path change

--- src/Host/eco-report-grid/src/ApiClient.js
+++ src/Host/eco-report-grid/src/ApiClient.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const ApiClient = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "http://35.200.190.55:8080"
+      ? "http://www.ecoservices.pk"
       : "http://localhost:5000"
 });
