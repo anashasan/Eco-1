84209cf9ae9c4472e0a0e5c72a0ae32f074a45ee
Khalil Mohammad <khalilmohammadmirza@gmail.com>
2018-11-25 18:06:57 +0500

    Updated react js api path

--- src/Host/eco-report-grid/src/ApiClient.js
+++ src/Host/eco-report-grid/src/ApiClient.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const ApiClient = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "http://www.ecoservices.pk"
+      ? "http://ecoservices.pk"
       : "http://localhost:5000"
 });
